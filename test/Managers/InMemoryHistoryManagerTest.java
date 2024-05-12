1247216db99a86d782b715a625167f376195cc96
AndrewKorolev <andreykorolevtulip@icloud.com>
2024-05-12 18:30:00 +0400

    Добавить неограниченную историю без повторов просмотров

--- test/Managers/InMemoryHistoryManagerTest.java
+++ test/Managers/InMemoryHistoryManagerTest.java
@@ -4,6 +4,8 @@ import TaskKindObjects.Task;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
@@ -21,5 +23,39 @@ class InMemoryHistoryManagerTest {
         Assertions.assertNotEquals(taskManager.receiveTask(task2.getId()), task1, "Задача в истории изменила " +
                 "свои данные после обновления");
     }
+
+    @Test
+    public void IfWeHaveCheckedTheSameTaskMoreThenOneTimeThatTaskShouldBeDeletedFromOldPlaceAndAddedToTheEndOfList(){
+        InMemoryHistoryManager history = new InMemoryHistoryManager();
+        Task task1 = new Task("Задача 1", "Первая задача", Status.NEW);
+        Task task2 = new Task("Задача 2", "Вторая задача", Status.NEW);
+        Task task3 = new Task("Задча 3", "Третья задача", Status.IN_PROGRESS);
+        InMemoryTaskManager manager = new InMemoryTaskManager();
+        manager.createNewTask(task1);
+        manager.createNewTask(task2);
+        manager.createNewTask(task3);
+        // создали задачи и задали им id при помощи менеджера
+
+        history.add(task1);
+        history.add(task2);
+        history.add(task3);
+        ArrayList<Task> oldList = history.getHistory();
+        // добавили задачи в историю и получили список истории
+
+        history.add(task1);
+        ArrayList<Task> newList = history.getHistory();
+        // просмотрели первую задачу еще раз - теперь она должна оказаться в конце списка - списки должны быть разные
+
+        Assertions.assertNotEquals(oldList, newList, "Списки одинаковые!");
+        System.out.println(oldList);
+        System.out.println(newList);
+
+        history.remove(task1.getId());
+        ArrayList<Task> listAfterRemoving = history.getHistory();
+        Assertions.assertNotEquals(listAfterRemoving, newList, "Списки одинаковые!");
+        System.out.println(listAfterRemoving);
+    }
+
+
 
 }
--- test/TaskKindObjects/EpicTest.java
+++ test/TaskKindObjects/EpicTest.java
@@ -1,8 +1,11 @@
 package TaskKindObjects;
 
 import Managers.InMemoryTaskManager;
+import Managers.TaskManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +19,26 @@ class EpicTest {
         Epic epic2 = new Epic("Эпик2","Эпик для проверки 2");
         epic2.setId(epic1.getId());
         Assertions.assertEquals(epic1.getId(), epic2.getId(), "id эпик не равны!");
+    }
+
+    @Test
+    public void ShouldNotHaveDeletedSubtask(){
+        Epic epic1 = new Epic("Эпик","Эпик для проверки");
+        InMemoryTaskManager manager = new InMemoryTaskManager();
+        manager.createNewEpic(epic1);
+        // создали эпик
+        Subtask subtask = new Subtask("Подзадача", "Подзадача для проверки", epic1);
+        manager.createNewSubtask(subtask);
+        // создали подзадачу и добавили в менеджер - теперь в списке подзадач эпика хранится эта подзадача
+        ArrayList<Subtask> forCheck = new ArrayList<>();
+        forCheck.add(subtask);
+        // создали такой же список для проверки
+        Assertions.assertEquals(epic1.getSubtasksOfSpecificEpic(), forCheck, "Списки не одинаковые!");
+        // проверили, что список подзадач эпика хранит данную подзадачу
+        manager.deleteSubtask(subtask.getId());
+        Assertions.assertNotEquals(epic1.getSubtasksOfSpecificEpic(), forCheck, "Списки одинаковые!");
+        // проверили, что список подзадач эпика теперь пустой и отличается от своей прошлой версии
+
     }
 
 }
